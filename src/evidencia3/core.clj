9a8d23d7a4111b739448f8ce2e8e2f84f19e5d58
Sylvia <a00825972@tec.mx>
2024-06-02 00:31:15 -0600

    read .txt

--- src/evidencia3/core.clj
+++ src/evidencia3/core.clj
@@ -1,4 +1,12 @@
-(ns evidencia3.core)
+(ns evidencia3.core
+  (:require [clojure.java.io :as io]))
 
 (defn foo []
   (println  "Hello, World!"))
+
+(defn leerArchivo [ruta]
+	(with-open [rdr (io/reader ruta)]
+		(let [contenido(slurp rdr)]
+			(read-string contenido))))
+(def inventario (leerArchivo "src/evidencia3/crucero.txt"))
+(println inventario)
